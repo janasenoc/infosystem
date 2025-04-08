ec893d0573deac1b29c32031a77d59fe87c6ba7c
janasenoc <janasenoc@gmail.com>
2025-04-08 18:07:18 +0800

    first commit

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,326 @@
+<template>
+  <div class="container" :class="{ 'right-panel-active': isSignUpActive }" id="container">
+    <div class="form-container sign-up-container">
+      <form @submit.prevent>
+        <h1>Create Account</h1>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Student ID" />
+        <input type="text" placeholder="Program" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+    <div class="form-container log-in-container">
+      <form @submit.prevent>
+        <h1>Log in</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <a href="#">Forgot your password?</a>
+        <button>Log In</button>
+      </form>
+    </div>
+    <div class="overlay-container">
+      <div class="overlay">
+        <div class="overlay-panel overlay-left">
+          <div class="logo-container">
+            <img :src="isLogo" alt="Logo" class="logo" />
+          </div>
+          <h1>Welcome Back!</h1>
+          <p>To keep connected with us please login with your personal info</p>
+          <button class="ghost" id="logIn" @click="switchToLogin">Log In</button>
+        </div>
+        <div class="overlay-panel overlay-right">
+          <div class="logo-container">
+            <img :src="isLogo" alt="Logo" class="logo" />
+          </div>
+          <h1>Hello, Ka-IS!</h1>
+          <p>Enter your personal details and start journey with us</p>
+          <button class="ghost" id="signUp" @click="switchToSignUp">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer>
+    <p>Created with <i class="fa fa-heart"></i> by: Jana Aera Senoc | Kimberly Suarez</p>
+  </footer>
+</template>
+
+<script>
+import isLogo from '@/assets/img/is.png'
+import loginBg from '@/assets/img/bg.jpg'
+
+export default {
+  name: 'LoginRegister',
+  data() {
+    return {
+      isSignUpActive: false,
+      isLogo,
+      loginBg,
+    }
+  },
+  methods: {
+    switchToSignUp() {
+      this.isSignUpActive = true
+    },
+    switchToLogin() {
+      this.isSignUpActive = false
+    },
+  },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,800');
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background: url('@/assets/img/bg.jpg') center/cover no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: 'Montserrat', sans-serif;
+  height: 100vh;
+  width: 100%;
+  margin: -20px 0 50px;
+}
+
+h1 {
+  font-weight: bold;
+  margin: 0;
+}
+
+p {
+  font-size: 14px;
+  font-weight: 100;
+  line-height: 20px;
+  letter-spacing: 0.5px;
+  margin: 20px 0 30px;
+}
+
+span {
+  font-size: 12px;
+}
+
+a {
+  color: #333;
+  font-size: 14px;
+  text-decoration: none;
+  margin: 15px 0;
+}
+
+button {
+  border-radius: 20px;
+  border: 1px solid #1dc5b7;
+  background-color: #1db6a1;
+  color: #ffffff;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 12px 45px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  transition: transform 80ms ease-in;
+}
+
+button:active {
+  transform: scale(0.95);
+}
+
+button:focus {
+  outline: none;
+}
+
+button.ghost {
+  background-color: transparent;
+  border-color: #ffffff;
+}
+
+form {
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 50px;
+  height: 100%;
+  text-align: center;
+}
+
+input {
+  background-color: #eee;
+  border: none;
+  padding: 12px 15px;
+  margin: 8px 0;
+  width: 100%;
+}
+
+.container {
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow:
+    0 14px 28px rgba(0, 0, 0, 0.25),
+    0 10px 10px rgba(0, 0, 0, 0.22);
+  position: relative;
+  overflow: hidden;
+  width: 768px;
+  max-width: 100%;
+  min-height: 480px;
+}
+
+.form-container {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  transition: all 0.6s ease-in-out;
+}
+
+.log-in-container {
+  left: 0;
+  width: 50%;
+  z-index: 2;
+}
+
+.container.right-panel-active .log-in-container {
+  transform: translateX(100%);
+}
+
+.sign-up-container {
+  left: 0;
+  width: 50%;
+  opacity: 0;
+  z-index: 1;
+}
+
+.container.right-panel-active .sign-up-container {
+  transform: translateX(100%);
+  opacity: 1;
+  z-index: 5;
+  animation: show 0.6s;
+}
+
+@keyframes show {
+  0%,
+  49.99% {
+    opacity: 0;
+    z-index: 1;
+  }
+  50%,
+  100% {
+    opacity: 1;
+    z-index: 5;
+  }
+}
+
+.overlay-container {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  transition: transform 0.6s ease-in-out;
+  z-index: 100;
+}
+
+.container.right-panel-active .overlay-container {
+  transform: translateX(-100%);
+}
+
+.overlay {
+  background: #27b6a3;
+  background: -webkit-linear-gradient(to right, #1bada6, #20afa8);
+  background: linear-gradient(to right, #1eb1a4, #16b3b3);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 0 0;
+  color: #ffffff;
+  position: relative;
+  left: -100%;
+  height: 100%;
+  width: 200%;
+  transform: translateX(0);
+  transition: transform 0.6s ease-in-out;
+}
+
+.container.right-panel-active .overlay {
+  transform: translateX(50%);
+}
+
+.logo-container {
+  position: absolute;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
+}
+
+.logo-container .logo {
+  width: 120px;
+  height: auto;
+}
+
+.overlay-panel {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 40px;
+  text-align: center;
+  top: 0;
+  height: 100%;
+  width: 50%;
+  transform: translateX(0);
+  transition: transform 0.6s ease-in-out;
+}
+
+.overlay-left {
+  transform: translateX(-20%);
+  align-items: center;
+  text-align: center;
+}
+
+.container.right-panel-active .overlay-left {
+  transform: translateX(0);
+}
+
+.overlay-right {
+  right: 0;
+  transform: translateX(0);
+  align-items: center;
+  text-align: center;
+}
+
+.container.right-panel-active .overlay-right {
+  transform: translateX(20%);
+}
+
+footer {
+  background-color: #0d7a71;
+  color: #fff;
+  font-size: 15px;
+  bottom: 0;
+  position: fixed;
+  left: 0;
+  right: 0;
+  text-align: center;
+  z-index: 999;
+}
+
+footer p {
+  margin: 10px 0;
+}
+
+footer i {
+  color: red;
+}
+
+footer a {
+  color: #3c97bf;
+  text-decoration: none;
+}
+</style>
